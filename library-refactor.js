85c72baaaccda02af45c1815b7c841ef5078f288
Nina Yujiri <ninayujiri@gmail.com>
2018-04-12 12:48:36 +0000

    cleaned up code

--- library-refactor.js
+++ library-refactor.js
@@ -13,7 +13,7 @@ var library = {
                    album: "Woodstock 1952"}
   },
   playlists: { p01: { id: "p01",
-                      name: "Coding Music",
+                      name: "Coding Music"
                       tracks: ["t01", "t02"]
                     },
                p02: { id: "p02",
@@ -24,7 +24,7 @@ var library = {
   printPlaylists: function () {
     for (var playlist in this.playlists) {
     this.playlistPrint(this.playlists[playlist].id);
-  }
+    }
   },
   playlistPrint: function (playlistId) {
     console.log(this.playlists[playlistId].id + ": " + this.playlists[playlistId].name + " - " + this.playlists[playlistId].tracks.length + " tracks");
@@ -37,12 +37,12 @@ var library = {
     console.log(playlistId + ": " + playlist.name + " - " + playlist.tracks.length + " tracks");
     for (var index in this.playlists[playlistId].tracks) {
     this.printTrack(this.playlists[playlistId].tracks[index]);
-  }
+    }
   },
   printTracks: function () {
     for (var track in this.tracks) {
     this.printTrack(this.tracks[track].id);
-  }
+    }
   },
   printTrack: function (trackId) {
     console.log(this.tracks[trackId].id + ": " + this.tracks[trackId].name + " by " + this.tracks[trackId].artist + " (" + this.tracks[trackId].album + ")");
@@ -69,17 +69,17 @@ var library = {
   },
  }
 
-// console.log(library.printPlaylists());
+var uid = function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
 
+// console.log(library.printPlaylists());
+//
 // library.addTrackToPlaylist('t03', 'p01');
 //
 // library.printPlaylist(library.playlists.p01.id);
 //
 // console.log(library.printTracks(library));
-//
-// var uid = function() {
-//   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-// }
 //
 // library.addTrack("Death Grips", "The Money Store", "Get Got");
 //
